659bcbcb6fef7f4fa0caa4dc98df624480d9162a
Valdis Locs <valdis.locs.web@gmail.com>
2020-01-30 22:58:51 +0200

    Add @isset($dish) or else throws error at dishes/create

--- resources/views/inc/nav.blade.php
+++ resources/views/inc/nav.blade.php
@@ -1,2 +1,4 @@
 <a href="{{ route('dishes.index') }}">/home</a>
-<a href="{{$dish->path('edit')}}">/edit {{$dish->name}}</a>
+@isset($dish)
+<a href="{{ $dish->path('edit') }}">/edit {{$dish->name}}</a>    
+@endisset
