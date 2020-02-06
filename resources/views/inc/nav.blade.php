2d502aa2cfee48ea014255904ce0ee71e5f4c91f
Valdis Locs <valdis.locs.web@gmail.com>
2020-02-06 19:44:38 +0200

    comented out edit link for $dish

--- resources/views/inc/nav.blade.php
+++ resources/views/inc/nav.blade.php
@@ -1,4 +1,5 @@
 <a href="{{ route('dishes.index') }}">/home</a>
 @isset($dish)
-<a href="{{ $dish->path('edit') }}">/edit {{$dish->name}}</a>    
+{{-- this is not working when route dishes.edit not defined --}}
+{{-- <a href="{{ $dish->path('edit')}}">/edit {{$dish->name}}</a>     --}}
 @endisset
